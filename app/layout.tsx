9faa3391dc26b8bfa44b98cc6996d725ad1f7105
Sofyann00 <akibsofyan521@gmail.com>
2025-06-03 13:54:22 +0700

    fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="light" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
